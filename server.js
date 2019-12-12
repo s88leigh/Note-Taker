90723d0f4b4ce12d652f245dbb9f4ddbf4474bd9
Sophie <sophie88leigh@gmail.com>
2019-12-12 18:35:52 -0500

    cleaned up folder/file routes

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 //Dependencies
-var http = require("http");
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
@@ -18,38 +17,43 @@ const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+//displays homepage
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+//displays the note's list
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+    console.log("your on note taker page");
+});
+
+//api route getting all the notes as json in the database
+app.get("/api/notes", (req, res) => {
+    // res.sendFile(path.join(__dirname, "db", "db.json"));
+    //  console.log("db");
+});
+
+//saving the notes to the database
+app.post("/api/notes", (req, res) => {
+    var newNotes = req.body;
+
+})
+
+//deleting notes from the database
+app.delete("api/notes/",(req, res) => {
+
+})
+
+// app.get("*", (req, res) => {
+
+// });
+
 //Listener will start our server
 app.listen(PORT, () => {
     console.log("the server is listening on PORT: " + PORT);
 });
-//Router
-//require("./routes/apiRoutes")(app);
-//require("./routes/htmlRoutes")(app);
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "Develop","public", "index.html")
-    );
-});
 
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "Develop", "public", "notes.html"));
-    console.log("your on note taker page");
-});
-
-// app.get("/api/notes", (req, res) => {
-//     res.sendFile(path.join(__dirname, "db", "db.json"));
-//      console.log("db");
-// });
-
-// app.post("/api/notes", (req, res) => {
-//     var newNotes = req.body;
-
-// })
-
-// app.delete("api/notes/",(req, res) => {
-
-// })
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "public\index.html"));
-//     console.log("your index");
-// });
-
+//to do:
+//delete route needs to be able to pass an id into it; look at last weeks notes about creating id route parameters
+//need to create new route for serving static files for css and index.js
