23527999f8f1cb268ba664522091499187a613d6
Sophie <sophie88leigh@gmail.com>
2019-12-15 01:26:56 -0500

    added code to delete items

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 //Sets up initial app to handle data parsing
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
@@ -31,14 +31,14 @@ app.get("/notes", (req, res) => {
 //api route getting all the notes as json in the database
 
 app.get("/api/notes", (err, res) => {
-    let newNotes; 
+    let newNotes;
     try {
-        newNotes=fs.readFileSync("db/db.json", "utf8");
+        newNotes = fs.readFileSync("db/db.json", "utf8");
         newNotes = JSON.parse(newNotes);
         console.log(newNotes);
-     
+
     }
-    catch (err){
+    catch (err) {
         console.log("\n error(in app.get.catch):");
         console.error(err);
     }
@@ -46,56 +46,50 @@ app.get("/api/notes", (err, res) => {
 
 });
 
-let newNotes = [];
-
 //saving the notes to the database
 app.post("/api/notes", (req, res) => {
- 
+
     try {
 
-        newNotes =fs.readFileSync("db/db.json", "utf8");
+        newNotes = fs.readFileSync("db/db.json", "utf8");
 
-        newNotes =JSON.parse(newNotes);
-       //adds notes to the body
+        newNotes = JSON.parse(newNotes);
+
+        //adds notes to the body
         newNotes.push(req.body)
 
         newNotes = JSON.stringify(newNotes);
+
         //Json turns parsed notes into a string for user
-        fs.writeFile("db/db.json", newNotes, "utf8", function(err) {
+        fs.writeFile("db/db.json", newNotes, "utf8", function (err) {
             if (err) throw err;
         });
 
         console.log(newNotes);
-     
+
     }
-    catch (err){
+    catch (err) {
         console.log("\n error(in app.post.catch):");
         console.error(err);
     }
     res.json(JSON.parse(newNotes));
 
 });
-    
 
-//   res.sendFile(path.join(__dirname, "public", "notes.html"));
-   
 
-//deleting notes from the database
-app.delete("api/notes/:id",(req, res) => {
-  
-    myArray.forEach(function(item){ delete item.id });
-    res.send('DELETE NOTES!');
+//deletes notes from the database
+app.delete("/api/notes/:id", (req, res) => {
+
+    let newNotes = []
+    newNotes.splice(req.params.id);
+    fs.writeFile("db/db.json", JSON.stringify(newNotes), "utf8", function (err) {
+        console.log(err);
+    });
+    res.json(newNotes);
 });
-
 
 //Listener will start our server
 app.listen(PORT, () => {
     console.log("the server is listening on PORT: " + PORT);
 });
 
-//to do:
-//delete route needs to be able to pass an id into it; look at last weeks notes about creating id route parameters
-//connect to db.json file
-//write code to push new notes to db
-
-//progress:  index, notes pages displays with css.  
